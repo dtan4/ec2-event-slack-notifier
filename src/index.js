2be9cdaa468278eb181f0f4522deabda446ed135
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-14 01:55:44 +0900

    Write function skeleton

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import AWS from 'aws-sdk';
+
+exports.handler = (event, context, callback) => {
+  let ec2 = new AWS.EC2();
+  let describeInstanceStatusPromise = ec2.describeInstanceStatus().promise();
+
+  describeInstanceStatusPromise.then((data) => {
+    let targets = data.InstanceStatuses.filter((v) => { v.events.length > 0 });
+    callback(null, targets)
+  }).catch((err) => {
+    callback("error", err);
+  });
+};
---
+++ src/run.js
@@ -0,0 +1,15 @@
+import Index from './index.js';
+
+var event = {};
+var context = {};
+var callback = (error, message) => {
+  if (error) {
+    console.log("ERROR!");
+    console.log(message);
+  } else {
+    console.log("SUCCESS!");
+    console.log(message);
+  }
+}
+
+Index.handler(event, context, callback);
