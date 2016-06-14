ce5ad5847ff84cfc11ac5066007f1c5843d67a85
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-14 12:34:19 +0900

    Fetch events properly

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ exports.handler = (event, context, callback) => {
   let ec2 = new AWS.EC2();
   let describeInstanceStatusPromise = ec2.describeInstanceStatus().promise();
 
-  describeInstanceStatusPromise.then((data) => {
-    let targets = data.InstanceStatuses.filter((v) => { v.events.length > 0; });
+  describeInstanceStatusPromise.then(data => {
+    let targets = data.InstanceStatuses.filter(v => v.Events.length > 0);
     callback(null, targets);
-  }).catch((err) => {
+  }).catch(err => {
     callback('error', err);
   });
 };
