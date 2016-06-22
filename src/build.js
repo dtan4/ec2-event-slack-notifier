e1b8aa3f40e2a6350709a3d6ffce7732c7959687
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-22 18:27:11 +0900

    Rename function to camelCase

--- src/build.js
+++ src/build.js
@@ -1,15 +1,15 @@
 import { exec } from 'child_process';
 import Package from '../package';
 
-function exclude_path(key) {
+function excludePath(key) {
   return `node_modules/${key}\\*`
 }
 
-function exclude_paths() {
-  return Object.keys(Package.devDependencies).map(key => exclude_path(key)).join(' ');
+function excludePaths() {
+  return Object.keys(Package.devDependencies).map(key => excludePath(key)).join(' ');
 }
 
-var command = `zip -r dist/ec2-event-slack-notifier.zip env.js index.js node_modules -x ${exclude_paths()}`
+var command = `zip -r dist/ec2-event-slack-notifier.zip env.js index.js node_modules -x ${excludePaths()}`
 
 exec(command, { maxBuffer: 400 * 1024 }, (err, stdout, stderr) => {
   console.log(`stdout: ${stdout}`);
