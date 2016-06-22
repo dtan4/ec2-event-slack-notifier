a68e9088f5ef0215da0c25353e50d3e2fce1a859
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-22 16:43:44 +0900

    Use prefilled context

--- src/run.js
+++ src/run.js
@@ -1,14 +1,40 @@
 import Index from './index.js';
 
-var event = {};
-var context = {};
-var callback = (error, message) => {
+const event = {};
+
+const context = {
+  succeed: (result) => {
+    console.log('SUCCESS!');
+    console.log(result);
+    process.exit(0);
+  },
+  fail: (error) => {
+    console.log('ERROR!');
+    console.log(error);
+    process.exit(1);
+  },
+  done: (err, res) => err ? fail(err) : succeed(res),
+  getRemainingTimeInMillis: () => Infinity,
+  functionName: "fakeLambda",
+  functionVersion: "0",
+  invokedFunctionArn: "arn:aws:lambda:fake-region:fake-acc:function:fakeLambda",
+  memoryLimitInMB: Infinity,
+  awsRequestId: "fakeRequest",
+  logGroupName: "fakeGroup",
+  logStreamName: "fakeStream",
+  identity: null,
+  clientContext: null
+};
+
+const callback = (error, message) => {
   if (error) {
     console.log('ERROR!');
     console.log(error);
+    process.exit(1);
   } else {
     console.log('SUCCESS!');
     console.log(message);
+    process.exit(0);
   }
 };
 
