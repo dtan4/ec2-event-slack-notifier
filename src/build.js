833cf5de907610b87c17120cd04c18b532612449
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-22 18:32:33 +0900

    Make cosmetic changes

--- src/build.js
+++ src/build.js
@@ -2,14 +2,14 @@ import { exec } from 'child_process';
 import Package from '../package';
 
 function excludePath(key) {
-  return `node_modules/${key}\\*`
+  return `node_modules/${key}\\*`;
 }
 
 function excludePaths() {
   return Object.keys(Package.devDependencies).map(key => excludePath(key)).join(' ');
 }
 
-var command = `zip -r dist/ec2-event-slack-notifier.zip env.js index.js node_modules -x ${excludePaths()}`
+var command = `zip -r dist/ec2-event-slack-notifier.zip env.js index.js node_modules -x ${excludePaths()}`;
 
 exec(command, { maxBuffer: 400 * 1024 }, (err, stdout, stderr) => {
   console.log(`stdout: ${stdout}`);
