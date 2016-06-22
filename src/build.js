b94b14f1df1ebcf0de5bf86819c06ba65be29c5a
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-22 17:26:01 +0900

    Build zip package with npm run

---
+++ src/build.js
@@ -0,0 +1,21 @@
+import { exec } from 'child_process';
+import Package from '../package';
+
+function exclude_path(key) {
+  return `node_modules/${key}\\*`
+}
+
+function exclude_paths() {
+  return Object.keys(Package.devDependencies).map(key => exclude_path(key)).join(' ');
+}
+
+var command = `zip -r dist/ec2-event-slack-notifier.zip lib/index.js node_modules -x ${exclude_paths()}`
+
+exec(command, { maxBuffer: 400 * 1024 }, (err, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+
+  if (err != null) {
+    console.log(`exec error: ${err}`);
+  }
+});
