7628c118d6704adef4f74bde723eda1654e42f7a
Daisuke Fujita <dtanshi45@gmail.com>
2016-10-19 02:35:34 +0900

    Add test skeleton

---
+++ test/slack_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let assert = require('power-assert');
+let slack = require('../slack.js');
+
+describe('slack', () => {
+  describe('#constructAttachments', () => {
+    it('should return Slack attachment', () => {
+      assert(1 === 2);
+    });
+  });
+});
